578934c641b0d53a4b73f9f1a4f2a21c7e543c2a
abelkent <abel.wbkent@gmail.com>
2022-11-04 12:33:48 +0000

    Updated and redid
    - Previous version was not functioning
    - redid this
    - Continue at later date

--- Day 15/AoC Day 15.py
+++ Day 15/AoC Day 15.py
@@ -1,101 +1,83 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 25 16:12:20 2022
+Created on Fri Nov  4 12:06:08 2022
 
 @author: abelw
 """
 import time
+
+#God am I sick of pathfinding, return to later
 
 def sol_a(filepath):
     
     with open(filepath) as file:
         data = file.readlines()
         data = [line.strip("\n") for line in data]
-        board = [ [int(x) for x in line] for line in data]
+        data = [[x for x in line.strip()] for line in data]
     
 
-    #point = [Y][X]
-    
-    def recursive_pathfinder(current_location, visited_points, current_risk):
-        #print(visited_points)
-        
-        def get_all_neighbours(point):
-            #             Top , Bottom , Left , Right
-            neighbours = [None, None, None, None]
-            
-            #Along top row
-            if point[0] != 0:
-                neighbours[0] = [point[0]-1,point[1]]
-            
-            #Along bottom row
-            if point[0] != len(board)-1:
-                neighbours[1] = [point[0]+1, point[1]]
-            
-            #Along left wall
-            if point[1] != 0:
-                neighbours[2] = [point[0], point[1]-1]
-            
-            if point[1] != (len(board[0])-1):
-                neighbours[3] = [point[0], point[1]+1]
-        
-        
-            while None in neighbours:
-                neighbours.remove(None)
-            
-            #print(neighbours)
-            
-            return(neighbours)
-        
-        
-        def get_valid_neighbours(neighbours):
-            valid_neighbours = []
-            
-            for neighbour in neighbours:
-                if neighbour not in visited_points:
-                    valid_neighbours.append(neighbour)
-            
-            return(valid_neighbours)
-        
-                
-        def check_current_point():
-            time.sleep(.1)
-            local_risk = board[current_location[0]][current_location[1]] + current_risk
-            
-            local_neighbours = get_all_neighbours(current_location)
-            valid_local_neighbours = get_valid_neighbours(local_neighbours)
-            #print(valid_local_neighbours)
-            
-            if current_location == [len(board),len(board[0])-1]:
-                print("REACHED END")
-                return local_risk
-            
-            elif len(valid_local_neighbours) == 0:
-                print("NO VALID NEIGHBOURS")
-                return 500000
-            
-            else:
-                paths = []
-                new_visited_points = list(visited_points)
-                new_visited_points.append(current_location)
-                for neighbour in valid_local_neighbours:
-                    local_path_score = recursive_pathfinder(neighbour, new_visited_points, local_risk)
-                    paths.append(local_path_score)
-            
-            
-            if len(paths) == None:
-                print("WTF")
-            
-            else:
-            
-                print(paths)
-                return min(paths)
-        
-        check_current_point()
-            
-        
-        
-    recursive_pathfinder([0,0],[],0)
-        #Reaches end
-        #Runs out of places to go
+    def get_value(point):
+        return int(data[point[0]][point[1]])
 
+    print(get_value([0,0]))
+    
+    def get_all_neighbours(point, visited_points):
+        
+        neighbours  = [None, None, None, None]
+        #Top
+        if point[0] != 0:
+            neighbours[0] = [point[0]-1,point[1]]
+        
+        #Bottom
+        if point[0] != len(data)-1:
+            neighbours[1] = [point[0]+1,point[1]]
+        
+        #Left
+        if point[1] != 0:
+            neighbours[2] = [point[0],point[1]-1]
+        
+        #Right
+        if point[1] != len(data[0])-1:
+            neighbours[3] = [point[0],point[1]+1]
+        
+        #Removes blank spaces from edge of board
+        while None in neighbours:
+            neighbours.remove(None)
+        
+        
+        #Removes previously visited points
+        valid_neighbours = []
+        for neighbour in neighbours:
+            if neighbour not in visited_points:
+                valid_neighbours.append(neighbour)
+        
+        return(valid_neighbours)
+    
+    def pathfinder(point, goal, current_risk, previously_visited):
+        
+        new_risk = current_risk + get_value(point) #Calculates new risk
+        
+        #Reaches goal
+        if point == goal:
+            print(new_risk)
+            return new_risk
+    
+        #Has run out of places to go
+        neighbours = get_all_neighbours(point, previously_visited)
+        if len(neighbours) == 0:
+            return None
+        
+        #Checks other paths and return minimum
+        new_path_values = []
+        visited_including_current = list(previously_visited)
+        visited_including_current.append(point)
+        for neighbour in neighbours:
+            new_path_values.append(pathfinder(neighbour, goal, new_risk, visited_including_current))
+        print(new_path_values)
+        
+        
+        time.sleep(1)
+    
+    pathfinder([0,0],[0,5], 0, [])
+          
 test = sol_a("test.txt")
